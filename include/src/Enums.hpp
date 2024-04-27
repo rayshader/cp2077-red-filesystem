c58d5000733f94f42dc5888caca6c5a58e54110a
Louis Poirier <contact@louispoirier.io>
2024-04-27 16:35:47 +0200

    fix: map RTTI type of enums.

--- include/src/Enums.hpp
+++ include/src/Enums.hpp
@@ -1,6 +1,8 @@
 #ifndef REDFILESYSTEM_ENUMS_HPP
 #define REDFILESYSTEM_ENUMS_HPP
 
+#include <RedLib.hpp>
+
 namespace RedFileSystem {
 
 enum class FileSystemStatus {
@@ -16,5 +18,8 @@ enum class FileSystemWriteMode {
 };
 
 }
+
+RTTI_MAP_TYPE_NAME(RedFileSystem::FileSystemStatus, "RedFileSystem.FileSystemStatus")
+RTTI_MAP_TYPE_NAME(RedFileSystem::FileSystemWriteMode, "RedFileSystem.FileSystemWriteMode")
 
 #endif  //REDFILESYSTEM_ENUMS_HPP
