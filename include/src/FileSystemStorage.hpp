c525fda2c15b12f8f16bbdaa507298b0adbff879
Louis Poirier <contact@louispoirier.io>
2024-05-08 21:47:19 +0200

    feat(api): support v0.7.0.

--- include/src/FileSystemStorage.hpp
+++ include/src/FileSystemStorage.hpp
@@ -3,6 +3,7 @@
 
 #include <RED4ext/RED4ext.hpp>
 #include <RedLib.hpp>
+#include <vector>
 
 #include "Enums.hpp"
 #include "File.hpp"
@@ -40,6 +41,18 @@ class FileSystemStorage {
 
     Red::CallVirtual(wrapper, "GetFile", file, path);
     return File(file);
+  }
+
+  inline std::vector<File> GetFiles() const {
+    Red::DynArray<Red::Handle<Red::IScriptable>> handles;
+
+    Red::CallVirtual(wrapper, "GetFiles", handles);
+    std::vector<File> files;
+
+    for (const auto& handle : handles) {
+      files.emplace_back(handle);
+    }
+    return files;
   }
 };
 
